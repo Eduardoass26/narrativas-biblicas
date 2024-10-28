4cf6823eeda8d04adb1b1c64a1010ac2b611bf6e
Eduardoass26 <eduardo026contato@gmail.com>
2024-10-28 19:59:03 -0300

    primeiro commit

---
+++ slider.js
@@ -0,0 +1,62 @@
+const images = [
+    { src: "../public/imgs/imgs_slider/jesus.png", name: "Jesus" },
+    { src: "../public/imgs/imgs_slider/moises-sarsa-ardente.png", name: "Moisés e a Sarsa Ardente" },
+    { src: "../public/imgs/imgs_slider/slider (2).webp", name: "Do inicio ao fim dos tempos" },
+    { src: "../public/imgs/imgs_slider/slider.webp", name: "A criação até o Apocalipse" },
+    { src: "../public/imgs/imgs_slider/davi.png", name: "Davi" },
+    { src: "../public/imgs/imgs_slider/egypt.png", name: "Egito Antigo" },
+    { src: "../public/imgs/imgs_slider/sansao.png", name: "Sansão" },
+    { src: "../public/imgs/imgs_slider/tomb-8622722.jpg", name: "Túmulo de Jesus" },
+    { src: "../public/imgs/imgs_slider/tomb-8563707.jpg", name: "Túmulo do Salvador" },
+    { src: "../public/imgs/imgs_slider/noahs-ark-8769317.png", name: "Arca de Noé" },
+    { src: "../public/imgs/imgs_slider/apocalipse.png", name: "Apocalipse" },
+    { src: "../public/imgs/imgs_slider/ai-generated-8772687.png", name: "O deserto" },
+    { src: "../public/imgs/imgs_slider/ai-generated-8701833.png", name: "As piramides do Egito" },
+    { src: "../public/imgs/imgs_slider/ai-generated-8701728.png", name: "Salomão" }
+];
+
+let currentSlide = 0;
+
+function initializeSlider() {
+  const sliderContainer = document.getElementById('sliderContainer');
+  
+  images.forEach((image, index) => {
+    const slide = document.createElement('div');
+    slide.classList.add('slide');
+    if (index === 0) slide.classList.add('active'); // Define o primeiro slide como ativo
+
+    const img = document.createElement('img');
+    img.src = image.src;
+    img.alt = image.name;
+    img.style.width = '100%'; // Ajusta o tamanho conforme necessário
+    img.style.height = 'auto';
+
+    slide.appendChild(img);
+    sliderContainer.appendChild(slide);
+  });
+}
+
+function showSlide(index) {
+  const slides = document.querySelectorAll('.slide');
+  
+  if (index >= slides.length) {
+    currentSlide = 0;
+  } else if (index < 0) {
+    currentSlide = slides.length - 1;
+  } else {
+    currentSlide = index;
+  }
+
+  slides.forEach((slide) => slide.classList.remove('active'));
+  slides[currentSlide].classList.add('active');
+}
+
+function changeSlide(direction) {
+  showSlide(currentSlide + direction);
+}
+
+// Inicializa o slider ao carregar a página
+document.addEventListener('DOMContentLoaded', () => {
+  initializeSlider();
+  showSlide(currentSlide);
+});
